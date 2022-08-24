adc2e0222dcec3104dd0dad4ad3966a6bd5ae9fe
GMatrixGames <gmatrixgames@hotmail.com>
2022-08-24 19:29:49 -0400

    Init

---
+++ DexOfCards/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DexOfCards;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddWpfBlazorWebView();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
+    }
+}
